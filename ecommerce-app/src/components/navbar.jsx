d28661bcf88e3f4b9f8c8a111071a571de6bab79
nclaira <nclaire981@gmail.com>
2025-10-18 12:27:40 +0200

    Initial commit: add Vite project files and update navbar

---
+++ ecommerce-app/src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import {Link} from "react-router-dom"
+
+function Navbar(){
+
+    return(
+        <>
+
+        <nav className="flex flex-between gap-[450px] px-32">
+
+            <div>
+                <h1>E-commerce</h1>
+            </div>
+            <div className="flex flex-between gap-12">
+                <Link to='/'>Home</Link>
+                <Link to='/products'>Products</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/contact'>Contact</Link>
+            </div>
+
+        </nav>
+        </>
+    )
+
+}
+
+export default Navbar;
