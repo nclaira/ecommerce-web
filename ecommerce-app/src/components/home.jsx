c9e7df59a869eb4aadce1f451f9c79bb8b5344dc
nclaira <nclaire981@gmail.com>
2025-10-19 00:55:37 +0200

    linking shop now button to the  products

--- ecommerce-app/src/components/home.jsx
+++ ecommerce-app/src/components/home.jsx
@@ -1,14 +1,18 @@
+import { Link } from 'react-router-dom'
+
 function Home(){
     return(
         
         <section className="text-center py-16 bg-gray-50">
       <h1 className="text-4xl font-bold text-[#128B47] mb-4">Welcome to BrightBuy Shop</h1>
       <p className="text-gray-700 mb-6">Your one-stop store for quality fashion, electronics, and accessories.</p>
-      <button
-        className="bg-[#128B47] text-white px-5 py-2 rounded hover:bg-green-700"
+      
+       <Link
+        to="/products"
+        className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700"
       >
-        Learn More
-      </button>
+        Shop Now
+      </Link>
     </section>
     )
 }
