28ff8a48b377bb48859716a234a5d7ffa713cbc8
nclaira <nclaire981@gmail.com>
2025-11-04 13:17:04 +0200

    create ecommerce-app, install and configure tailwind css

---
+++ ecommerce-app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-center text-green-600">
+      My E-Commerce App is Running!
+    </h1>
+  )
+}
+
+export default App
