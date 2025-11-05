06cdd54c0cfe75aad96eb5b3b15db18ef4640303
nclaira <nclaire981@gmail.com>
2025-11-05 13:52:32 +0200

    Update App.jsx

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,8 +1,30 @@
+import './App.css'
+import './index.css'
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'  
+import Home from './pages/home'
+import Navbar from './components/navbar'
+import About from './pages/about'
+import Products from './pages/products'
+import Contact from './pages/contact'
+import Footer from './components/footer'
+
 function App() {
+  
   return (
-    <h1 className="text-3xl font-bold underline text-center text-green-600">
-      My E-Commerce App is Running!
-    </h1>
+    <Router>
+      <div className="app">
+        <Navbar/> 
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/contact" element={<Contact />} /> 
+          </Routes>
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   )
 }
 
